311855214b55e46e876fc3f943a4c7e7666b9abe
PK <nozumaz@gmail.com>
2023-07-05 08:24:44 -0400

    outputs fibonacci sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+    fib = []
+    for i in range(length):
+        if (i < 2):
+            fib.append(i)
+        else:
+            fib.append(fib[i-1] + fib[i-2])
+
+    print(fib)
